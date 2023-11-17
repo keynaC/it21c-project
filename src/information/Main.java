a7f5616e8af9e7b84bba31ccbcf146fcdcf90c2b
keynaC <keynagogo251@gmail.com>
2023-11-17 14:24:41 +0800

    added keyna

---
+++ src/information/Main.java
@@ -0,0 +1,12 @@
+
+package information;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am keyna");
+     }
+    
+}
+    
+
---
+++ src/information/profile.java
@@ -0,0 +1,13 @@
+
+package information;
+
+
+public class profile {
+    public static void main(String[] args) {
+        System.out.println("profile");
+     }
+    
+}
+
+    
+
