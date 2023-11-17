ae6b0e1bd7841df2558754e719c37323975a6c41
keynaC <keynagogo251@gmail.com>
2023-11-17 14:35:01 +0800

    added food package

---
+++ src/foodfav.java
@@ -0,0 +1,11 @@
+
+public class foodfav {
+    public static void main(String[] args) {
+        System.out.println("food");
+     }
+    
+    
+}
+
+    
+
---
+++ src/softdrinks.java
@@ -0,0 +1,7 @@
+
+public class softdrinks {
+    public static void main(String[] args) {
+        System.out.println("coke");
+     }
+    
+}
